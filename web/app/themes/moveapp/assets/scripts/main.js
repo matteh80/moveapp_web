0fa83e78013404b92aed229ce46a0ca3be5316c1
Mathias Hedström <mathias@timedev.se>
2015-06-10 16:11:20 +0200

    Footer w facebook-status

--- web/app/themes/moveapp/assets/scripts/main.js
+++ web/app/themes/moveapp/assets/scripts/main.js
@@ -10,81 +10,115 @@
  * always reference jQuery with $, even when in .noConflict() mode.
  * ======================================================================== */
 
-(function($) {
+(function ($) {
 
-  // Use this variable to set up the common and page specific functions. If you
-  // rename this variable, you will also need to rename the namespace below.
-  var Sage = {
-    // All pages
-    'common': {
-      init: function() {
-        // JavaScript to be fired on all pages
-      },
-      finalize: function() {
-        // JavaScript to be fired on all pages, after page specific JS is fired
-      }
-    },
-    // Home page
-    'home': {
-      init: function() {
-        // JavaScript to be fired on the home page
-      },
-      finalize: function() {
-        $("video").click(function(e){
-          $("video").not(this).each(function(ele) {
-            video = this;
-            this.pause();
-            $(this).siblings(".play-btn").css("display", "block");
-            $(this).siblings(".genericon-fullscreen").css("display", "none");
-            $(this).removeAttr("controls");
-          });
+    // Use this variable to set up the common and page specific functions. If you
+    // rename this variable, you will also need to rename the namespace below.
+    var Sage = {
+        // All pages
+        'common': {
+            init: function () {
+                // JavaScript to be fired on all pages
+            },
+            finalize: function () {
+                // JavaScript to be fired on all pages, after page specific JS is fired
+            }
+        },
+        // Home page
+        'home': {
+            init: function () {
+                // JavaScript to be fired on the home page
+            },
+            finalize: function () {
+                $("video").click(function (e) {
+                    $("video").not(this).each(function (ele) {
+                        video = this;
+                        this.pause();
+                        $(this).siblings(".play-btn").css("display", "block");
+                        $(this).siblings(".genericon-fullscreen").css("display", "none");
+                        $(this).removeAttr("controls");
+                    });
 
-          // toggles play / pause
-          this.paused ? this.play() : this.pause();
-          this.paused ? $(this).siblings(".play-btn").css("display", "block") : $(this).siblings(".play-btn").css("display", "none");
-          this.paused ? $(this).removeAttr("controls", "") : $(this).attr("controls", "");
-        });
-      }
-    },
-    // About us page, note the change from about-us to about_us.
-    'about_us': {
-      init: function() {
-        // JavaScript to be fired on the about us page
-      }
-    }
-  };
+                    // toggles play / pause
+                    this.paused ? this.play() : this.pause();
+                    this.paused ? $(this).siblings(".play-btn").css("display", "block") : $(this).siblings(".play-btn").css("display", "none");
+                    this.paused ? $(this).removeAttr("controls", "") : $(this).attr("controls", "");
+                });
 
-  // The routing fires all common scripts, followed by the page specific scripts.
-  // Add additional events for more control over timing e.g. a finalize event
-  var UTIL = {
-    fire: function(func, funcname, args) {
-      var fire;
-      var namespace = Sage;
-      funcname = (funcname === undefined) ? 'init' : funcname;
-      fire = func !== '';
-      fire = fire && namespace[func];
-      fire = fire && typeof namespace[func][funcname] === 'function';
+                //FACEBOOK
+                debug_js = '//connect.facebook.net/en_US/sdk/debug.js';
+                js = '//connect.facebook.net/sv_SE/sdk.js';
 
-      if (fire) {
-        namespace[func][funcname](args);
-      }
-    },
-    loadEvents: function() {
-      // Fire common init JS
-      UTIL.fire('common');
+                $.getScript(debug_js, function () {
+                    FB.init({
+                        appId: '332897300242814',
+                        appSecret: 'a912eb6e7d0a4136f0ae9d799258818b',
+                        cookie: true,
+                        xfbml: false,  // parse social plugins on this page
+                        version: 'v2.3' // or v2.0, v2.1, v2.0
+                    });
 
-      // Fire page-specific init JS, and then finalize JS
-      $.each(document.body.className.replace(/-/g, '_').split(/\s+/), function(i, classnm) {
-        UTIL.fire(classnm);
-        UTIL.fire(classnm, 'finalize');
-      });
 
-      // Fire common finalize JS
-      UTIL.fire('common', 'finalize');
-    }
-  };
+                    //FB.api("/oauth/access_token",
+                    //    {   client_id: '332897300242814',
+                    //        client_secret: 'a912eb6e7d0a4136f0ae9d799258818b',
+                    //        grant_type: 'fb_exchange_token',
+                    //        fb_exchange_token: '332897300242814|qGg9igkAKsL88273AqbGmifBAtQ'
+                    //    }, function(response) {
+                    //        console.log(response)
+                    //    })
 
-  // Load Events
-  $(document).ready(UTIL.loadEvents);
+                    var accessToken = '332897300242814|qGg9igkAKsL88273AqbGmifBAtQ';
+                    FB.api('1632026917030663/feed', {
+                        access_token : accessToken,
+                        status_type: 'shared_story'
+                    }, function(response) {
+                        console.log(response)
+                        $('#fb-text').append(response.data[0].description);
+                    });
+                });
+
+            }
+        },
+        // About us page, note the change from about-us to about_us.
+        'about_us': {
+            init: function () {
+                // JavaScript to be fired on the about us page
+            }
+        }
+    };
+
+    // The routing fires all common scripts, followed by the page specific scripts.
+    // Add additional events for more control over timing e.g. a finalize event
+    var UTIL = {
+        fire: function (func, funcname, args) {
+            var fire;
+            var namespace = Sage;
+            funcname = (funcname === undefined) ? 'init' : funcname;
+            fire = func !== '';
+            fire = fire && namespace[func];
+            fire = fire && typeof namespace[func][funcname] === 'function';
+
+            if (fire) {
+                namespace[func][funcname](args);
+            }
+        },
+        loadEvents: function () {
+            // Fire common init JS
+            UTIL.fire('common');
+
+            // Fire page-specific init JS, and then finalize JS
+            $.each(document.body.className.replace(/-/g, '_').split(/\s+/), function (i, classnm) {
+                UTIL.fire(classnm);
+                UTIL.fire(classnm, 'finalize');
+            });
+
+            // Fire common finalize JS
+            UTIL.fire('common', 'finalize');
+        }
+    };
+
+    // Load Events
+    $(document).ready(UTIL.loadEvents);
 
 })(jQuery); // Fully reference jQuery after this point.
--- web/app/themes/moveapp/templates/footer.php
+++ web/app/themes/moveapp/templates/footer.php
@@ -1,5 +1,32 @@
 <footer class="content-info" role="contentinfo">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
+    <div class="container menu">
+        <div class="col-md-3 logo">
+            <a class="brand" href="<?= esc_url(home_url('/')); ?>">
+                <img src="<?php bloginfo('stylesheet_directory'); ?>/dist/images/moveapplogo.png"
+                     class="img-responsive"/>
+            </a>
+        </div>
+        <div class="col-md-9 footer-menu">
+            <?php dynamic_sidebar('sidebar-footer'); ?>
+        </div>
+    </div>
+    <div class="row social">
+        <div class="container">
+            <div class="col-md-6 fb">
+                <div class="col-xs-2 image-container">
+                    <img src="<?php bloginfo('stylesheet_directory'); ?>/dist/images/fb_icon.png" class="img-responsive" />
+                </div>
+                <div class="col-xs-8 content">
+                    <p id="fb-text"></p>
+                </div>
+                <div class="col-xs-2">
+                    <i class="glyphicon glyphicon-chevron-right"></i>
+                </div>
+            </div>
+            <div class="col-md-6">
+
+            </div>
+        </div>
+    </div>
 </footer>
+
