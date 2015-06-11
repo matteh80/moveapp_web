86301f1e09e37a0df7ced7e1ad8bdba5108f6b1f
Mathias Hedström <mathias@timedev.se>
2015-06-11 17:26:54 +0200

    ...

--- web/app/themes/moveapp/lib/faq-post-type.php
+++ web/app/themes/moveapp/lib/faq-post-type.php
@@ -28,8 +28,8 @@ function custom_post_faq() {
             'query_var' => true,
             'menu_position' => 8, /* this is what order you want it to appear in on the left hand side menu */
             'menu_icon' => 'dashicons-welcome-learn-more', /* the icon for the custom post type menu */
-            'rewrite'	=> array( 'slug' => 'faq', 'with_front' => false ), /* you can specify its url slug */
-            'has_archive' => 'faq', /* you can rename the slug here */
+            'rewrite'	=> array( 'slug' => 'faqs', 'with_front' => false ), /* you can specify its url slug */
+            'has_archive' => false, /* you can rename the slug here */
             'capability_type' => 'post',
             'hierarchical' => false,
             /* the next one is important, it tells what's enabled in the post editor */
