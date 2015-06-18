02dadb17856453d2cd4557a645a9fed17985396e
Mathias Hedström <mathias@timedev.se>
2015-06-18 18:32:09 +0200

    test

--- web/app/themes/moveapp/assets/scripts/remote-api.js
+++ web/app/themes/moveapp/assets/scripts/remote-api.js
@@ -30,10 +30,8 @@ var jsonUser;
 function login(data){
     $.ajax({
         url: apiUrl+'api-token-auth/',
-        contentType: "application/json",
         method: "POST",
         data: JSON.stringify(data),
-        crossDomain: true,
         processData: false,
         dataType: 'json',
         success:function(response){
