b47617b242479723d58c3ac4108d59cc5db8d569
Mathias Hedström <mathias@timedev.se>
2015-06-18 16:51:24 +0200

    Start contact

---
+++ web/app/themes/moveapp/template-contact.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Contact Template
+ */
+?>
+<?php get_template_part('templates/page', 'header'); ?>
+
+<div class="row">
+
+</div>
+
+
+
+
+
+<?php the_posts_navigation(); ?>
