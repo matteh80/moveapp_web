a63fb61641000847cd0a173bfd51f6d24c7edced
Mathias Hedström <mathias@timedev.se>
2015-06-22 11:52:09 +0200

    Center header on profile

--- web/app/themes/moveapp/lib/config.php
+++ web/app/themes/moveapp/lib/config.php
@@ -57,7 +57,7 @@ function display_sidebar()
                 'is_front_page',
                 ['is_page_template', 'template-custom.php'],
                 ['is_page_template', 'template-faq.php'],
-                ['is_page_template', 'template-user.php'],
+                ['is_page_template', 'template-profil.php'],
                 ['is_singular', 'faq']
             ]
         );
