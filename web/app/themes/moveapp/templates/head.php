0951fe04316e22ba33abfd669f52d310c42a9eb3
Mathias Hedström <mathias@timedev.se>
2015-06-24 11:57:13 +0200

    Facebook something something

--- web/app/themes/moveapp/templates/head.php
+++ web/app/themes/moveapp/templates/head.php
@@ -5,4 +5,20 @@
     <?php wp_head(); ?>
     <meta name="google-site-verification" content="3NYSjp7cioYXHYqsm_B8ZeI3sJBBgFxXzu5JB4oag8o"/>
     <script src='https://www.google.com/recaptcha/api.js'></script>
+    <script>(function() {
+            var _fbq = window._fbq || (window._fbq = []);
+            if (!_fbq.loaded) {
+                var fbds = document.createElement('script');
+                fbds.async = true;
+                fbds.src = '//connect.facebook.net/en_US/fbds.js';
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(fbds, s);
+                _fbq.loaded = true;
+            }
+            _fbq.push(['addPixelId', '1632732436965177']);
+        })();
+        window._fbq = window._fbq || [];
+        window._fbq.push(['track', 'PixelInitialized', {}]);
+    </script>
+    <noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?id=1632732436965177&amp;ev=PixelInitialized" /></noscript>
 </head>
