7726949cccb982c05173f59abbbb7749d166490c
Mathias Hedström <mathias@timedev.se>
2015-08-14 12:40:51 +0200

    ...

--- web/app/themes/moveapp/home.php
+++ web/app/themes/moveapp/home.php
@@ -2,9 +2,11 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-                <h1>Workout made easy!</h1>
-                <h4 class="col-md-5"><p>100-tals träningspass - hur, var och när du vill - i mobilen, paddan eller på TV:n</p>
-                <p>99 kr i månaden, en gratis prova-på-månad. Inga bindningstider</p></h4>
+                <div class="row">
+                    <h1 class="col-xs-12">Workout made easy!</h1>
+                    <h4 class="col-md-5"><p>100-tals träningspass - hur, var och när du vill - i mobilen, paddan eller på TV:n</p>
+                        <p>99 kr i månaden, en gratis prova-på-månad. Inga bindningstider</p></h4>
+                </div>
             </div>
         </div>
         <div class="row">
