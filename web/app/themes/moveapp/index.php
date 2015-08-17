073a4c63e0a584da0a4ec9097b1e58342eee5474
Mathias Hedström <mathias@timedev.se>
2015-08-17 13:53:45 +0200

    ...

--- web/app/themes/moveapp/index.php
+++ web/app/themes/moveapp/index.php
@@ -1,6 +1,5 @@
 <?php get_template_part('templates/page', 'header'); ?>
 <?php while (have_posts()) : the_post(); ?>
-    gfdgfdgfd
   <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
 <?php endwhile; ?>
 
