dcb456cc6cd89320e02aea9d8f91cdfa1a013820
Mathias Hedström <mathias@timedev.se>
2015-09-11 14:47:08 +0200

    Facebook pixel

--- web/app/themes/moveapp/templates/head.php
+++ web/app/themes/moveapp/templates/head.php
@@ -5,7 +5,7 @@
     <?php wp_head(); ?>
     <meta name="google-site-verification" content="3NYSjp7cioYXHYqsm_B8ZeI3sJBBgFxXzu5JB4oag8o"/>
     <script src='https://www.google.com/recaptcha/api.js'></script>
-    <script>(function() {
+    <script>(function () {
             var _fbq = window._fbq || (window._fbq = []);
             if (!_fbq.loaded) {
                 var fbds = document.createElement('script');
@@ -20,11 +20,50 @@
         window._fbq = window._fbq || [];
         window._fbq.push(['track', 'PixelInitialized', {}]);
     </script>
-    <noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?id=1632732436965177&amp;ev=PixelInitialized" /></noscript>
+    <noscript><img height="1" width="1" alt="" style="display:none"
+                   src="https://www.facebook.com/tr?id=1632732436965177&amp;ev=PixelInitialized"/></noscript>
     <!-- Go to www.addthis.com/dashboard to customize your tools -->
-    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-558c00fa7497fddc" async="async"></script>
+    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-558c00fa7497fddc"
+            async="async"></script>
     <script type="text/javascript">
         var templateUrl = '<?= get_bloginfo("template_url"); ?>';
     </script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+    <!-- Facebook Pixel Code -->
+    <script>
+        !function (f, b, e, v, n, t, s) {
+            if (f.fbq)return;
+            n = f.fbq = function () {
+                n.callMethod ?
+
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq)f._fbq = n;
+
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window,
+
+            document, 'script', '//connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '432703740261045');
+
+        fbq('track', 'PageView');
+    </script>
+
+    <noscript><img height="1" width="1" style="display:none"
+
+                   src="https://www.facebook.com/tr?id=432703740261045&ev=PageView&noscript=1"
+
+            /></noscript>
+    <!-- End Facebook Pixel Code -->
+    
 </head>
