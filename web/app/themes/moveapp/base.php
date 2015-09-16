c221ae6ea199e441e37cf4e5973f2315b9625e8b
Mathias Hedström <mathias@timedev.se>
2015-09-16 10:58:45 +0200

    Google analytics

--- web/app/themes/moveapp/base.php
+++ web/app/themes/moveapp/base.php
@@ -9,6 +9,16 @@ use Roots\Sage\Wrapper;
 <html class="no-js" <?php language_attributes(); ?>>
   <?php get_template_part('templates/head'); ?>
   <body <?php body_class(); ?>>
+  <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-64470227-2', 'auto');
+      ga('send', 'pageview');
+
+  </script>
   <!-- Google Tag Manager -->
   <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MN743R"
                     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
