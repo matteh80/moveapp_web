30e1a33dfb3a0871c6e4cd0c83c9139719bb7860
Mathias Hedström <mathias@timedev.se>
2015-09-16 11:26:43 +0200

    ,..

--- web/app/themes/moveapp/templates/head.php
+++ web/app/themes/moveapp/templates/head.php
@@ -5,15 +5,6 @@
     <?php wp_head(); ?>
     <meta name="google-site-verification" content="3NYSjp7cioYXHYqsm_B8ZeI3sJBBgFxXzu5JB4oag8o"/>
     <script src='https://www.google.com/recaptcha/api.js'></script>
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-64470227-2', 'auto');
-        ga('send', 'pageview');
-
     </script>
 
     <script>(function () {
